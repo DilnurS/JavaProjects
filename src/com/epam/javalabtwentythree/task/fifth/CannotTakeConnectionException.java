f9ef85fc813891bbfa95f9bcdb4b80b251545949
DilnurS <dilnur@rambler.ru>
2017-12-30 12:51:34 +0600

    Strange things

---
+++ src/com/epam/javalabtwentythree/task/fifth/CannotTakeConnectionException.java
@@ -0,0 +1,10 @@
+package com.epam.javalabtwentythree.task.fifth;
+
+
+@SuppressWarnings("serial")
+
+public class CannotTakeConnectionException extends Throwable {
+    public CannotTakeConnectionException() {
+        super();
+    }
+}
